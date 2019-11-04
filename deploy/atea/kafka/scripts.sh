295444a1f0fe1f48ee01ea8ff76b968cfff384e1
airenas <airenass@gmail.com>
2019-11-04 11:50:50 +0200

    Update helper kafka script

--- deploy/atea/kafka/scripts.sh
+++ deploy/atea/kafka/scripts.sh
@@ -3,3 +3,6 @@ kafka-consumer-groups --bootstrap-server kafka-svc.atea.svc.cluster.local:9092 -
 
 ## consume all messages
 kafka-console-consumer --bootstrap-server kafka-svc.atea.svc.cluster.local:9092 --group Transcriber.Service.Group --topic NewAudioAvailable
+
+## show log
+ watch "kubectl logs $(kubectl get po | grep -e '^kafka' | tail -n 3| head -n 1 | awk '{print $1}') | tail -n 50"
