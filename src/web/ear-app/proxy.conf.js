5b7570f27674df58a1a6eac6a9f97d3b55184a15
airenas <airenass@gmail.com>
2019-11-26 11:21:59 +0200

    Update default proxy URLS

--- src/web/ear-app/proxy.conf.js
+++ src/web/ear-app/proxy.conf.js
@@ -1,20 +1,20 @@
 const PROXY_CONFIG = {
   "/transcriber/": {
-    "target": "http://localhost:80/ausis.1",
+    "target": "http://localhost:80/ausis",
     "secure": false
     //"pathRewrite": {"^/transcriber": ""}
   },
   "/status.service/": {
-    "target": "http://localhost:80/ausis.1",
+    "target": "http://localhost:80/ausis",
     "secure": false
     //"pathRewrite": {"^/result.provider": ""}
   },
   "/result.service/": {
-    "target": "http://localhost:80/ausis.1",
+    "target": "http://localhost:80/ausis",
     "secure": false
   },
   "/status.service/subscribe": {
-    "target": "ws://localhost:80/ausis.1",
+    "target": "ws://localhost:80/ausis",
     "secure": false,
     "ws": true,
     "logLevel": "debug"
