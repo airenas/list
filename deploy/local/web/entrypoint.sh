54524a697efe0e66ba0a5cf672241fb7728beb4d
airenas <airenass@gmail.com>
2020-03-25 12:55:25 +0200

    Refactor to makefile

--- deploy/local/web/entrypoint.sh
+++ deploy/local/web/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo Starting
 sed -i "s|<base href=\"/\">|<base href=\"$BASE_HREF\">|" /usr/share/nginx/html/index.html
-
+echo Updated base ref to $BASE_HREF
+echo Starting nginx
 nginx -g 'daemon off;'
