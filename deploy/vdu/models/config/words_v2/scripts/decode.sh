f5cc9f802b253790c9ce037b24573521aa229f53
airenas <airenass@gmail.com>
2020-04-06 13:15:52 +0300

    Add decode preload script

--- deploy/vdu/models/config/words_v2/scripts/decode.sh
+++ deploy/vdu/models/config/words_v2/scripts/decode.sh
@@ -25,26 +25,27 @@ datadir=$3
 decodedir=$4
 
 ## execute decoding
+echo "============= execute real decoding script ==================="
+online_ivectors="'scp:$ivectorsdir/ivector_online.scp'"
+feats="'ark,s,cs:apply-cmvn --norm-means=false --norm-vars=false --utt2spk=ark:$datadir/split$nj/1/utt2spk scp:$datadir/split$nj/1/cmvn.scp scp:$datadir/split$nj/1/feats.scp ark:- |'"
+lat_wspecifier="'ark:|lattice-scale --acoustic-scale=10.0 ark:- ark:- | gzip -c >$decodedir/lat.1.gz'"
+
 ## execute decoding
 echo "============= execute real decoding script ==================="
-
-feats="ark,s,cs:apply-cmvn --norm-means=false --norm-vars=false --utt2spk=ark:$datadir/split$nj/JOB/utt2spk scp:$datadir/split$nj/JOB/cmvn.scp scp:$datadir/split$nj/JOB/feats.scp ark:- |"
-lat_wspecifier="ark:|lattice-scale --acoustic-scale=10.0 ark:- ark:- | gzip -c >$decodedir/lat.JOB.gz"
-
-$cmd --num-threads $num_threads JOB=1:$nj $decodedir/log/decode.JOB.log \
-  nnet3-latgen-faster-parallel \
-     --num-threads=$num_threads \
-     --online-ivectors=scp:"$ivectorsdir"/ivector_online.scp \
-     --online-ivector-period=10 \
-     --frame-subsampling-factor=3 \
-     --frames-per-chunk=90 \
-     --extra-left-context=40 \
-     --extra-right-context=40 \
-     --extra-left-context-initial=0 \
-     --extra-right-context-final=0 \
-     --minimize=false --max-active=7000 --min-active=200 --beam=15 \
-     --lattice-beam=8 --acoustic-scale=1.0 --allow-partial=true \
-     --word-symbol-table=$graphdir/words.txt $graphdir/final.mdl \
-     $graphdir/HCLG.fst "$feats" "$lat_wspecifier" || exit 1;
+./pipe.runner -i pipe_input -o pipe_output -t 3m $online_ivectors $feats $lat_wspecifier || exit 1;
+  # nnet3-latgen-faster-parallel \
+  #    --num-threads=$num_threads \
+  #    --online-ivectors=scp:"$ivectorsdir"/ivector_online.scp \
+  #    --online-ivector-period=10 \
+  #    --frame-subsampling-factor=3 \
+  #    --frames-per-chunk=90 \
+  #    --extra-left-context=40 \
+  #    --extra-right-context=40 \
+  #    --extra-left-context-initial=0 \
+  #    --extra-right-context-final=0 \
+  #    --minimize=false --max-active=7000 --min-active=200 --beam=15 \
+  #    --lattice-beam=8 --acoustic-scale=1.0 --allow-partial=true \
+  #    --word-symbol-table=$graphdir/words.txt $graphdir/final.mdl \
+  #    $graphdir/HCLG.fst "$feats" "$lat_wspecifier" || exit 1;
 echo "============= done ==================="
 exit 0;
