bb8630b0d2c0882d1f668006966e73ff3e545017
airenas <airenass@gmail.com>
2020-09-09 19:59:33 +0300

    Add tests to result-make scripts

--- src/decode/scripts/m_end.sh
+++ src/decode/scripts/m_end.sh
@@ -2,7 +2,7 @@
 echo $(date +'%Y-%m-%d %T'): END ${tsk}
 if [ -n "$tsk" ] 
 then
-    ${APP_DIR}/send.metric -w ${worker} -t ${tsk} -i ${id};
+    ${SEND_METRIC_APP} -w ${worker} -t ${tsk} -i ${id};
 fi;    
 
 
--- src/decode/scripts/m_start.sh
+++ src/decode/scripts/m_start.sh
@@ -2,5 +2,5 @@
 echo $(date +'%Y-%m-%d %T'): START $1   
 if [ -n "$tsk" ] 
 then
-    ${APP_DIR}/send.metric -s -w ${worker} -t ${tsk} -i ${id};
+    ${SEND_METRIC_APP} -s -w ${worker} -t ${tsk} -i ${id};
 fi;  
