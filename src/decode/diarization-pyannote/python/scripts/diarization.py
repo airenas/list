0aef563a099a24d7d16ff4caf059f87c606c8a16
Airenas Vaičiūnas <airenass@gmail.com>
2023-10-17 15:50:04 +0300

    Make diarization docker for pyannote wrapper

---
+++ src/decode/diarization-pyannote/python/scripts/diarization.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+import sys
+import time
+
+import torch
+from pyannote.audio import Pipeline
+
+from scripts.logger import logger
+
+
+def main(argv):
+    logger.info("Starting")
+    parser = argparse.ArgumentParser(description="Pyannote diarization")
+    parser.add_argument("--input", nargs='?', required=True, help="wav file")
+    parser.add_argument("--output", nargs='?', required=True, help="rttm file")
+    parser.add_argument("--num-speakers", nargs='?', required=False, help="speakers count")
+    args = parser.parse_args(args=argv)
+
+    logger.info("Init models")
+    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1", use_auth_token=os.getenv('HF_API_TOKEN'))
+    cuda = os.getenv('CUDA')
+    if cuda and cuda != "cpu":
+        pipeline = pipeline.to(torch.device(cuda))
+    logger.info(f"Starting diarization on '{cuda}'")
+
+    start_time = time.time()
+    diarization = pipeline(args.input, num_speakers=args.num_speakers)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    logger.info(f"Done diarization in {elapsed_time:.2f}s")
+
+    with open(args.output, "w") as f:
+        diarization.write_rttm(f)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ src/decode/diarization-pyannote/python/scripts/rttm_to_seg.py
@@ -0,0 +1,53 @@
+import argparse
+import os
+import sys
+
+from scripts.logger import logger
+
+
+class Seg:
+    def __init__(self, start, dur, sp):
+        self.sp = sp
+        self.dur = float(dur)
+        self.start = float(start)
+
+
+def main(argv):
+    logger.info("Starting")
+    parser = argparse.ArgumentParser(description="Converts rttm to kaldi seg format")
+    parser.add_argument("--input", nargs='?', required=True, help="Input file to parse")
+    parser.add_argument("--output", nargs='?', required=True, help="Output file")
+    args = parser.parse_args(args=argv)
+
+    file = args.input
+    out_file = args.output
+    file_name, _ = os.path.splitext(os.path.basename(out_file))
+    logger.info(f"In: {file}")
+    logger.info(f"Out: {out_file}")
+
+    segs = []
+    with open(file, "r") as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                splits = line.split(" ")
+                segs.append(Seg(start=splits[3], dur=splits[4], sp=splits[7]))
+
+    segs = sorted(segs, key=lambda d: (d.sp, d.start))
+
+    rttm_lines = []
+    for s in segs:
+        start_time = int(s.start * 100)
+        duration = int(s.dur * 100)
+        label = s.sp
+
+        rttm_line = f"prepared 1 {start_time} {duration} NA NA NA {label}"
+        rttm_lines.append(rttm_line)
+
+    with open(out_file, "w") as file:
+        [file.write(line + '\n') for line in rttm_lines]
+    logger.info("done")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
